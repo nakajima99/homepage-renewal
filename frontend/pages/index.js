640f912657d8a72aee9c57674e23918cc7bbdd0e
numagami123 <numagami@interbond.net>
2022-06-10 13:35:55 +0900

    hogenumaaaa

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,6 @@ import { Button } from '@mui/material'
 
 export default function Home() {
   return (
-    <Button variant="outlined">test</Button>
+    <Button variant="outlined">numaaaaaaaaaaaa</Button>
   )
 }

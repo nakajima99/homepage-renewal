7124781e0d98005ebae31a14da597a36ba19d0af
nakajima99 <105795572+nakajima99@users.noreply.github.com>
2022-08-07 02:35:32 +0900

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,6 @@ import { Button } from '@mui/material'
 
 export default function Home() {
   return (<>
-    <Button variant="outlined"></Button>
+    <Button variant="outlined">interbond</Button>
   </>)
 }
